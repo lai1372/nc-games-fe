1d0d4fb305a43a6e42d6805f2ffa89d00b79049a
Laila Case <lailacase@Lailas-MacBook-Pro.local>
2023-05-30 13:45:21 +0100

    Remove bullet points from li, add border styling to ul, make button clearer and add loading state

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,19 +3,26 @@ import { fetchReviews } from "../../utils";
 
 const Home = () => {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchReviews().then((reviews) => {
       setReviews(reviews.reviews);
+    })
+    .then(()=>{
+        setIsLoading(false);
     });
   }, [reviews]);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <section className="home">
-      <h2>Reviews</h2>
+      <h2>All reviews ({reviews.length})</h2>
       {reviews.map((review) => {
         return (
-          <ul>
+          <ul className="review-list">
             <li>Title: {review.title}</li>
             <li> Category: {review.category}</li>
             <li> By: {review.owner}</li>
