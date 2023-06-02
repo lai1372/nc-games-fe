8ba5da09bd51fe6d41c5604a20cc901df13f0c9f
Laila Case <lailacase@Lailas-MacBook-Pro.local>
2023-06-02 12:21:46 +0100

    add category filter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
 import { useEffect, useState } from "react";
-import { fetchReviews } from "../../utils";
+import {
+  fetchReviews,
+  fetchCategories,
+  fetchReviewsByCategory,
+} from "../../utils";
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categories, setCategories] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedCategory, setSelectedCategory] = useState("")
+
+
+  useEffect(() => {
+    fetchCategories()
+      .then((reviewsByCat) => {
+        setCategories(reviewsByCat);
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
+  }, [selectedCategory]);
 
   useEffect(() => {
     fetchReviews()
@@ -13,14 +32,56 @@ const Home = () => {
       .then(() => {
         setIsLoading(false);
       });
-  }, [reviews]);
+  }, []);
+// console.log("hi")
+  const searchByCategory = (event) => {
+    setSelectedCategory(event.target.value)
+    setIsLoading(true)
+    fetchReviewsByCategory(event.target.value).then((reviews) => {
+      setIsLoading(false)
+      if (reviews.filtered_reviews){
+        setReviews(reviews.filtered_reviews);
+      }
+      // const categoryQuery = searchParams.get('category')
+      // const newParams = new URLSearchParams(searchParams)
+      // newParams.set('category', event.target.value)
+      // setSearchParams(newParams)
+    });
+  };
+
+  const clearFilter = () => {
+    fetchReviews().then((reviews) => {
+      setReviews(reviews.reviews);
+    });
+  };
+
+  const formatter = (category) => {
+    category = category.replace(/-/g, ' ');
+  return category.charAt(0).toUpperCase() + category.slice(1);
+}
+  
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
+
   return (
     <section className="home">
-      <h2>All reviews ({reviews.length})</h2>
+      <p>Select category</p>
+      <button onClick={clearFilter}>All Reviews</button>
+      {categories.map((category) => {
+        return (
+          <button
+            onClick={searchByCategory}
+            key={category.slug}
+            value={category.slug}
+          >
+            {formatter(category.slug)}
+          </button>
+        );
+      })}
+      {console.log(reviews)}
+      <h3>Reviews ({reviews.length})</h3>
       {reviews.map((review) => {
         return (
           <section key={review.review_id} className="review-list">
